407938f7d21fe9214fc5ae5de2546ef379f8bc22
Maryrau2201 <93242768+Maryrau2201@users.noreply.github.com>
2022-05-26 18:29:17 +0300

    Module3 Task 3 and 4

---
+++ CourseApp/Module3/FindSubstring.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseApp.Module3
+{
+    public class FindSubstring
+    {
+        public static long Get_hash(string s, int n, int p, int x)
+        {
+            long res = 0;
+            for (int e = 0; e < n; e++)
+            {
+                res = ((res * x) + s[e]) % p;
+            }
+
+            return res;
+        }
+
+        public static void Rabin_karp(string s, string t, int p, int x)
+        {
+            long ht = Get_hash(t, t.Length, p, x);
+
+            long hs = Get_hash(s, t.Length, p, x);
+
+            long xt = 1;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                xt = (xt * x) % p;
+            }
+
+            for (int e = 0; e <= s.Length - t.Length; e++)
+            {
+                if (ht == hs)
+                {
+                    Console.Write("{0} ", e);
+                }
+
+                if (e + t.Length < s.Length)
+                {
+                    hs = ((hs * x) - (s[e] * xt) + s[e + t.Length]) % p;
+                    hs = (hs + p) % p;
+                }
+            }
+        }
+
+        public static void EnterValues()
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+
+            int p = 1000000000 + 7;
+            int x = 26;
+
+            Rabin_karp(s, t, p, x);
+        }
+    }
+}
---
+++ CourseApp/Module3/PeriodString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CourseApp.Module3
+{
+    public class PeriodString
+    {
+        public static int[] Prefix_function(string s)
+        {
+            int[] res = new int[s.Length];
+            res[0] = 0;
+
+            for (int e = 0; e < s.Length - 1; e++)
+            {
+                int b = res[e];
+
+                while (b > 0 && s[e + 1] != s[b])
+                {
+                    b = res[b - 1];
+                }
+
+                if (s[e + 1] == s[b])
+                {
+                    res[e + 1] = b + 1;
+                }
+                else
+                {
+                    res[e + 1] = 0;
+                }
+            }
+
+            return res;
+        }
+
+        public static void EnterValues()
+        {
+            string s = Console.ReadLine();
+
+            int[] prefixs = Prefix_function(s);
+
+            int result = s.Length - prefixs[s.Length - 1];
+
+            if (s.Length % result == 0)
+            {
+                Console.WriteLine(s.Length / result);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
