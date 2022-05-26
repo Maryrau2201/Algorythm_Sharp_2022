050f70ff7d97898dd62fa6f9e3a8a794bbbb4744
Maryrau2201 <93242768+Maryrau2201@users.noreply.github.com>
2022-05-26 18:49:57 +0300

    Node для Module5, Task 1 and 2

---
+++ CourseApp/Module5/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseApp.Module5
+{
+    public class Node
+    {
+        public Node(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+        public int Data { get; set; }
+    }
+}
