aacb70b6da2b83a60e68451c540abb0bacec4235
Maryrau2201 <93242768+Maryrau2201@users.noreply.github.com>
2022-05-26 18:29:33 +0300

    Module 3 Task 1 and 2

---
+++ CourseApp/Module3/CircularString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CourseApp.Module3
+{
+    public class CircularString
+    {
+        public static int[] Prefix_function(string s)
+        {
+            int[] res = new int[s.Length];
+            res[0] = 0;
+
+            for (int e = 0; e < s.Length - 1; e++)
+            {
+                int b = res[e];
+
+                while (b > 0 && s[e + 1] != s[b])
+                {
+                    b = res[b - 1];
+                }
+
+                if (s[e + 1] == s[b])
+                {
+                    res[e + 1] = b + 1;
+                }
+                else
+                {
+                    res[e + 1] = 0;
+                }
+            }
+
+            return res;
+        }
+
+        public static void EnterValues()
+        {
+            string s = Console.ReadLine();
+
+            int[] prefixs = Prefix_function(s);
+
+            int result = s.Length - prefixs[s.Length - 1];
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CourseApp/Module3/CyclicShift.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace CourseApp.Module3
+{
+    public class CyclicShift
+    {
+        public static int RabKar(string b, string t)
+        {
+            if (b == t)
+            {
+                return 0;
+            }
+
+            t = string.Concat(Enumerable.Repeat(t, 2));
+
+            long q = 13;
+            long m = 1;
+            long p = 10000000 - 7;
+
+            long first_hash = 0;
+            long second_hash = 0;
+            long xt = 1;
+
+            foreach (char e in b.Reverse())
+            {
+                first_hash = (first_hash + (e * m)) % p;
+                m = (m * q) % p;
+            }
+
+            m = 1;
+
+            for (int e = b.Length - 1; e >= 0; e--)
+            {
+                second_hash = (second_hash + (t[e] * m)) % p;
+                m = (m * q) % p;
+            }
+
+            for (int e = 0; e < b.Length - 1; e++)
+            {
+                xt = (xt * q) % p;
+            }
+
+            for (int e = 1; e < t.Length - b.Length + 1; e++)
+            {
+                if (first_hash == second_hash)
+                {
+                    return e - 1;
+                }
+
+                second_hash = q * (second_hash - (t[e - 1] * xt));
+                second_hash += t[e + b.Length - 1];
+                second_hash = second_hash % p;
+
+                if ((second_hash < 0 && p > 0) || (second_hash > 0 && p < 0))
+                {
+                    second_hash += p;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void EnterValues()
+        {
+            string b = Console.ReadLine();
+            string t = Console.ReadLine();
+
+            int result = RabKar(b, t);
+
+            Console.WriteLine(result);
+        }
+    }
+}
