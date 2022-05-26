b04066a2c3bb47d5046094b83fa7447257369f51
Maryrau2201 <93242768+Maryrau2201@users.noreply.github.com>
2022-05-26 18:49:17 +0300

    Module 5 Task 1 and 2

---
+++ CourseApp/Module5/BinaryTreeBranches.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseApp.Module5
+{
+    public class BinaryTreeBranches
+    {
+        private Node root;
+
+        public static void BinaryTreeMethod()
+        {
+            string s = Console.ReadLine();
+
+            string[] values = s.Split(' ');
+
+            var tree = new BinaryTreeBranches();
+
+            for (int e = 0; e < values.Length - 1; e++)
+            {
+                tree.Insert(int.Parse(values[e]));
+            }
+
+            tree.LaunchPrintLastNodes();
+        }
+
+        public void Insert(int data)
+        {
+            root = InnerInsert(data, root);
+        }
+
+        private void LaunchPrintLastNodes()
+        {
+            PrintLastNodes(root);
+        }
+
+        private void PrintLastNodes(Node value)
+        {
+            if (value == null)
+            {
+                return;
+            }
+
+            PrintLastNodes(value.Left);
+
+            if ((value.Left != null && value.Right == null) || (value.Right != null && value.Left == null))
+            {
+                Console.WriteLine(value.Data);
+            }
+
+            PrintLastNodes(value.Right);
+        }
+
+        private Node InnerInsert(int data, Node root)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+
+            if (root.Data > data)
+            {
+                root.Left = InnerInsert(data, root.Left);
+            }
+            else if (root.Data < data)
+            {
+                root.Right = InnerInsert(data, root.Right);
+            }
+
+            return root;
+        }
+    }
+}
---
+++ CourseApp/Module5/IsTreeBalanced.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseApp.Module5
+{
+    public class IsTreeBalanced
+    {
+        private Node root;
+
+        public static void IsTreeBalancedMethod()
+        {
+            string s = Console.ReadLine();
+
+            string[] values = s.Split(' ');
+
+            var tree = new IsTreeBalanced();
+
+            for (int e = 0; e < values.Length - 1; e++)
+            {
+                tree.Insert(int.Parse(values[e]));
+            }
+
+            if (tree.IsBalanced(tree.root))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        public void Insert(int data)
+        {
+            root = InnerInsert(data, root);
+        }
+
+        private Node InnerInsert(int data, Node root)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+
+            if (root.Data > data)
+            {
+                root.Left = InnerInsert(data, root.Left);
+            }
+            else if (root.Data < data)
+            {
+                root.Right = InnerInsert(data, root.Right);
+            }
+
+            return root;
+        }
+
+        private bool IsBalanced(Node node)
+        {
+            int lh;
+
+            int rh;
+
+            if (node == null)
+            {
+                return true;
+            }
+
+            lh = Height(node.Left);
+            rh = Height(node.Right);
+
+            if (Math.Abs(lh - rh) <= 1 && IsBalanced(node.Left)
+                && IsBalanced(node.Right))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private int Height(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(Height(node.Left), Height(node.Right));
+        }
+    }
+}
